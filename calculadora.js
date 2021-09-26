ce3f6b49d374164b1ead9d3ef899c2170cbebe26
JMoneytoday <88577395+JMoneytoday@users.noreply.github.com>
2021-09-26 15:29:54 +0100

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+function abrir() {
+    window.open("https://github.com/JMoneytoday/Calculadora-HTML");
+}
+
+let y = 0;
+function color() {
+    if (y == 0) {
+        document.getElementById("fondo").style.background = "#c0c0c0";
+        y = 1;
+    } else if (y == 1) {
+        document.getElementById("fondo").style.background = "#eeeeee";
+        y = 0;
+    }
+
+}
